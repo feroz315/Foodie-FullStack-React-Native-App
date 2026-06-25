e2c928bfe6d20bcfffbdf32f6ca3f4f7d8e17bcc
feroz315 <feroz3151@gmail.com>
2026-06-25 11:37:06 +0500

    new

---
+++ Frontend/App.jsx
@@ -0,0 +1,23 @@
+// import { Provider } from 'react-redux';
+// import Navigation from "./src/screens/navigation";
+// import { store } from "./src/state/Store";
+// import Toast from 'react-native-toast-message';
+import { Text } from "react-native";
+
+const App = () => {
+ return (
+<>
+{/* 
+  <Provider store={store}>    
+    <Navigation />
+       </Provider>
+      <Toast /> */}
+    
+      <Text>Welcome New Project</Text>
+</>
+ 
+   );
+}
+
+
+export default App;

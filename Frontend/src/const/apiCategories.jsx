8b77660f48a3e8d68f4f2ffaa1e94a9cf77e7bce
feroz315 <feroz3151@gmail.com>
2026-07-13 22:56:21 +0500

    updates

--- Frontend/src/const/apiCategories.jsx
+++ Frontend/src/const/apiCategories.jsx
@@ -1,64 +1,12 @@
-
-const categories = [
+ [
     
-    {
-     id: '1', 
-     name: 'Rolls', 
-     price:250,
-     description: "Classic delight with 100% real mozzarella cheese",
-     rating: 4.7,
-     qty: 0,
-     duration:"30-35 min",
-     icon: require('../assets/icons/ch-roll.png')
-
-    },
-    {
-      id: 2,
-      name: "Burger",
-      price:200,
-      description: "Classic delight with 100% real Burger",
-      rating: 4.2,
-      duration:"20-25 min",
-      icon: require('../assets/icons/burgers.png')
-  },
-  {
-      id: 3,
-      name: "Bar B Q",
-      price:450,
-      description: "Classic delight with 100% real Bar BQ",
-      rating: 4.2,
-      duration:"35-45 min",   
-      icon: require('../assets/icons/barbq.png')
-  },
-  {
-      id: 4,
-      name: "Handi",
-      price:650,
-      description: "Classic delight with 100% real mozzarella cheese",
-      rating: 4.1,
-      duration:"45-60 min",
-      icon: require('../assets/icons/handi.png')
-  },
-  {
-      id: 5,
-      name: "Karhai",
-      price:900,
-      description: "Classic delight with 100% real mozzarella cheese",
-      rating: 4.8,
-      duration:"50-55 min",
-      icon: require('../assets/icons/chicken-karahi.png')
-  },
-
-  {
-      id: 6,
-      name: "Fish",
-      price:1250,
-      description: "Classic delight with 100% real mozzarella cheese",
-      rating: 4.9,
-      duration:"30-35 min",
-      icon: require('../assets/icons/fish.png')
-  },
-  {
+   
+  
+   
+  
+  
+  
+  
       id: 7,
       name: "Dal\nSabzi",
       price:250,
@@ -66,7 +14,6 @@ const categories = [
       rating: 4.7,
       duration:"15-20 min",
       icon: require('../assets/icons/dal.png')
-  },
   {
       id: 8,
       name: "Paratha",
@@ -105,86 +52,211 @@ const categories = [
   
   ];
   
-const Restaurant = [
-    
-    {
-     id: '1', 
-     name: 'Al-Habib', 
-     description: "Al Habib Restaurant is presenting a smoothing and mesmerizing environment to eat with delight.",
-     rating: 4.7,
-     time:"Open 24-hour",
-     phonenumber:"0333-3378667",
-     address:"Karachi - Hyderabad Motorway,Gadap Town,Karachi City,Sindh",
-     icon: require('../assets/images/Restaurant/al-habib-restaurant.jpg')
 
-    },
-    {
-      id: 2,
-      name: "BBQ",
-      description: "Inaugurated on the 10th of November in 1988, B.B.Q. Tonight started serving authentic Pakistani dishes and its signature B.B.Q dishes.",
-      rating: 4.2,
-      icon: require('../assets/images/Restaurant/bbq-tonight-restaurant.jpg')
+
+
+
+  
+ [
+  {
+    "id": 1,
+    "title": "Rolls",
+    "price": "Rs.250",
+    "description": "Classic delight with 100% real mozzarella cheese",
+    "images": require('../assets/icons/ch-roll.png'),
+    "rating": 4.3,
+    "reviews": 35
+   
   },
   {
-      id: 3,
-      name: "Haveli",
-      description: "The Haveli Khalil Khan is in itself a unique representation of the rich heritage of Karachi.",
-      rating: 4.2,
-      icon: require('../assets/images/Restaurant/haveli-restaurant.jpg')
-  },
-  {
-      id: 4,
-      name: "Kolachi",
-      description: "Superior service, personalized attention.We started with the simple idea of bringing the best from us to you.",
-      rating: 4.1,
-      icon: require('../assets/images/Restaurant/kolachi-restaurant.jpg')
-  },
-  {
-      id: 5,
-      name: "LaL-Qila",
-      description: "A restaurant depicting the legendary grandeur of splendid and noble past. A vivid and conspicuous realization of the glory of our brilliant esthetical essence.",
-      rating: 4.8,
-      icon: require('../assets/images/Restaurant/lal-qila-restaurant.jpg')
+    "id": 2,
+    "title": "Burger",
+    "price": "Rs.200",
+    "description": "Classic delight with 100% real Burger",
+    "images": require('../assets/icons/burgers.png'),
+    "rating": 4.1,
+    "reviews": 14     
+        
   },
 
   {
-      id: 6,
-      name: "Saltanat",
-      description: "Welcome to the gastronomic kingdom of Saltanat Restaurant, where culinary delight meets regal splendor.",
-      rating: 4.9,
-      icon: require('../assets/images/Restaurant/saltanat-restaurant.jpg')
+    "id": 3,
+    "title": "Bar B-Q",
+    "price": "Rs.450",
+    "description": "Classic delight with 100% real Bar BQ",
+    "images": require('../assets/icons/barbq.png'),
+    "rating": 4.3,
+    "reviews": 42     
+  
   },
   {
-      id: 7,
-      name: "Shaheen Shinwari",
-      description: "In 2001, the restaurant founder, Mr. Haji Muhammad Hussain, set out on a mission to construct a dine-in food restaurant in the city of Lights Karachi, Pakistan.",
-      rating: 4.7,
-      time:"Open 24-hour",
-      phonenumber:"0333-3378667",
-      address:"Karachi - Hyderabad Motorway,Gadap Town,Karachi City,Sindh",
-      icon: require('../assets/images/Restaurant/shaheen-shinwari-restaurant.jpg')
+    "id": 4,
+    "title": "Handi",
+    "price": "Rs.1050",
+    "description": "Classic delight with 100% real mozzarella",
+    "images": require('../assets/icons/handi.png'),
+    "rating": 4.7,
+    "reviews": 27
+      
   },
   {
-      id: 8,
-      name: "Royal Taj",
-      description: "Royal Taj is best Restaurant with 100% delicious food recipes.",
-      rating: 3.8,
-      icon: require('../assets/images/Restaurant/royal-taj-menu.jpg')
+    "id": 5,
+    "title": "Karhai",
+    "price": "Rs.1200",
+    "description": "Classic delight with 100%",
+    "images": require('../assets/icons/chicken-karahi.png'),
+    "rating": 4.8,
+    "reviews": 31    
+  
   },
   {
-      id: 9,
-      name: "Chef Table",
-      price:150,
-      description: "Classic delight with 100% real Chef Table.",
-      rating: 4.7,
-      duration:"10-15 min",
-      icon: require('../assets/images/Restaurant/Chefs-Table-Pakistan-1024x683.jpg')
+    "id": 6,
+    "title": "Fish",
+    "price": "Rs.1250",
+    "description": "delight with 100% ",
+    "images": require('../assets/icons/fish.png'),
+    "rating": 4.8,
+    "reviews": 33
+  
+  
+   },
+  {
+    "id": 7,
+    "title": "Classic Red Baseball Cap",
+    "price": 35,
+    "description": "Elevate your casual wardrobe with this timeless red baseball cap. Crafted from durable fabric, it features a comfortable fit with an adjustable strap at the back, ensuring one size fits all. Perfect for sunny days or adding a sporty touch to your outfit.",
+    "category": "Clothes",
+    "images": "https://i.imgur.com/N1GkCIR.jpeg",
+    "currency_code":"USD",
+    "rating": 4.8,
+    "reviews": 124
+   
+   },
+  {
+    "id": 8,
+    "title": "Classic Olive Chino Shorts",
+    "price": 84,
+    "description": "Elevate your casual wardrobe with these classic olive chino shorts. Designed for comfort and versatility, they feature a smooth waistband, practical pockets, and a tailored fit that makes them perfect for both relaxed weekends and smart-casual occasions.",
+    "category": "Clothes",
+    "images": "https://i.imgur.com/UsFIvYs.jpeg",
+    "currency_code":"USD",
+    "rating": 4.8,
+    "reviews": 124
+   
+   },
+  {
+    "id": 9,
+    "title": "Classic High-Waisted Athletic Shorts",
+    "price": 43,
+    "description": "Stay comfortable and stylish with our Classic High-Waisted Athletic Shorts. Designed for optimal movement and versatility, these shorts are a must-have for your workout wardrobe. Featuring a figure-flattering high waist, breathable fabric.",
+    "category": "Clothes",
+    "images": "https://i.imgur.com/eGOUveI.jpeg",
+    "currency_code":"USD",
+    "rating": 4.8,
+    "reviews": 124
+  
+  },
+  {
+    "id": 10,
+    "title": "Classic White Crew Neck T-Shirt",
+    "price": 39,
+    "description": "Elevate your basics with this versatile white crew neck tee. Made from a soft, breathable cotton blend, it offers both comfort and durability. Its sleek, timeless design ensures it pairs well with virtually any outfit.",
+    "category": "Clothes",
+    "images": "https://i.imgur.com/axsyGpD.jpeg",
+    "currency_code":"USD",
+    "rating": 4.8,
+    "reviews": 124
   },
   
-  ];
+  {
+    "id": 11,
+    "title": "Sleek White & Orange Wireless Gaming Controller",
+    "price": 69,
+    "description": "Elevate your gaming experience with this state-of-the-art wireless controller, featuring a crisp white base with vibrant orange accents. Designed for precision play, the ergonomic shape and responsive buttons provide maximum comfort and control for endless hours of gameplay.",
+    "category": "Electronics",
+    "images": "https://i.imgur.com/ZANVnHE.jpeg",
+    "currency_code":"USD",
+    "rating": 4.8,
+    "reviews": 124
+    
+  },
+  {
+    "id": 12,
+    "title": "Sleek Wireless Headphone & Inked Earbud Set",
+    "price": 44,
+    "description": "Experience the fusion of style and sound with this sophisticated audio set featuring a pair of sleek, white wireless headphones offering crystal-clear sound quality and over-ear comfort.",
+    "category": "Electronics",
+    "images": "https://i.imgur.com/yVeIeDa.jpeg",
+    "currency_code":"USD",
+    "rating": 4.8,
+    "reviews": 124
+    
+  },
+  {
+    "id": 13,
+    "title": "Efficient 2-Slice Toaster",
+    "price": 48,
+    "description": "Enhance your morning routine with our sleek 2-slice toaster, featuring adjustable browning controls and a removable crumb tray for easy cleaning. This compact and stylish appliance is perfect for any kitchen, ensuring your toast is always golden brown and delicious.",
+    "category": "Electronics",
+    "images": "https://i.imgur.com/keVCVIa.jpeg",
+    "currency_code":"USD",
+    "rating": 4.8,
+    "reviews": 124
+    
+  },
+  {
+    "id": 14,
+    "title": "Sleek Wireless Computer Mouse",
+    "price": 15,
+    "description": "Experience smooth and precise navigation with this modern wireless mouse, featuring a glossy finish and a comfortable ergonomic design. Its responsive tracking and easy-to-use interface make it the perfect accessory for any desktop or laptop setup.",
+    "category": "Electronics",
+    "images": "https://i.imgur.com/w3Y8NwQ.jpeg",
+    "currency_code":"USD",
+    "rating": 4.8,
+    "reviews": 124
+    
+  },
+  {
+    "id": 15,
+    "title": "Sleek Modern Laptop with Ambient Lighting",
+    "price": 43,
+    "description": "Experience next-level computing with our ultra-slim laptop, featuring a stunning display illuminated by ambient lighting. This high-performance machine is perfect for both work and play, delivering powerful processing in a sleek, portable design.",
+    "category": "Electronics",
+    "images": "https://i.imgur.com/OKn1KFI.jpeg",
+    "currency_code":"USD",
+    "rating": 4.8,
+    "reviews": 124
+    
+  },
+  {
+    "id": 16,
+    "title": "Sleek Modern Laptop for Professionals",
+    "price": 97,
+    "description": "Experience cutting-edge technology and elegant design with our latest laptop model. Perfect for professionals on-the-go, this high-performance laptop boasts a powerful processor, ample storage, and a long-lasting battery life, all encased in a lightweight.",
+    "category": "Electronics",
+    "images": "https://i.imgur.com/ItHcq7o.jpeg",
+    "currency_code":"USD",
+    "rating": 4.8,
+    "reviews": 124
+    
+  },  
+  {
+   "id": 17,
+    "title": "Sleek Mirror Finish Phone Case",
+    "slug": "sleek-mirror-finish-phone-case",
+    "price": 27,
+    "description": "Enhance your smartphone's look with this ultra sleek mirror finish phone case.",
+    "category": "Electronics",
+    "images":   "https://i.imgur.com/yb9UQKL.jpeg",
+    "currency_code":"USD",
+    "rating": 4.8,
+    "reviews": 124
+ }
+        
+]
 
 
 
 
 
-  export {categories,Restaurant };
+

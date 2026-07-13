c6114952378a0b6f0b1221dc57238521df369a14
feroz315 <feroz3151@gmail.com>
2026-07-13 23:11:34 +0500

    new

--- Frontend/src/const/apiCategories.jsx
+++ Frontend/src/const/apiCategories.jsx
@@ -1,61 +1,3 @@
- [
-    
-   
-  
-   
-  
-  
-  
-  
-      id: 7,
-      name: "Dal\nSabzi",
-      price:250,
-      description: "Classic delight with 100% real mozzarella cheese",
-      rating: 4.7,
-      duration:"15-20 min",
-      icon: require('../assets/icons/dal.png')
-  {
-      id: 8,
-      name: "Paratha",
-      price:100,
-      description: "Classic delight with 100% real Paratha",
-      rating: 3.8,
-      duration:"10-15 min",
-      icon: require('../assets/icons/paratha.png')
-  },
-  {
-      id: 9,
-      name: "Sand\nWiches",
-      price:150,
-      description: "Classic delight with 100% real mozzarella cheese",
-      rating: 4.7,
-      duration:"10-15 min",
-      icon: require('../assets/icons/sandwich.png')
-  },
-  {
-      id: 10,
-      name: "French\nFries",
-      price:100,
-      description: "Classic delight with 100% real mozzarella cheese",
-      rating: 3.6,
-      duration:"8-12 min",
-      icon: require('../assets/icons/french-fries.png')
-  },
-  {
-      id: 11,
-      name: "Drinks",
-      price:150,
-      description: "Drinks with 100% real ",
-      rating: 4.7,
-      icon: require('../assets/icons/cold-drinks.png')
-  },
-  
-  ];
-  
-
-
-
-
   
  [
   {
@@ -117,67 +59,56 @@
     "images": require('../assets/icons/fish.png'),
     "rating": 4.8,
     "reviews": 33
-  
-  
+    
    },
   {
     "id": 7,
-    "title": "Classic Red Baseball Cap",
-    "price": 35,
-    "description": "Elevate your casual wardrobe with this timeless red baseball cap. Crafted from durable fabric, it features a comfortable fit with an adjustable strap at the back, ensuring one size fits all. Perfect for sunny days or adding a sporty touch to your outfit.",
-    "category": "Clothes",
-    "images": "https://i.imgur.com/N1GkCIR.jpeg",
-    "currency_code":"USD",
-    "rating": 4.8,
-    "reviews": 124
-   
+    "title": "Dal\nSabzi",
+    "price": "Rs.350",
+    "description": "Classic delight",
+    "images": require('../assets/icons/dal.png'),
+    "rating": 3.8,
+    "reviews": 14   
+
    },
   {
     "id": 8,
-    "title": "Classic Olive Chino Shorts",
-    "price": 84,
-    "description": "Elevate your casual wardrobe with these classic olive chino shorts. Designed for comfort and versatility, they feature a smooth waistband, practical pockets, and a tailored fit that makes them perfect for both relaxed weekends and smart-casual occasions.",
-    "category": "Clothes",
-    "images": "https://i.imgur.com/UsFIvYs.jpeg",
-    "currency_code":"USD",
-    "rating": 4.8,
-    "reviews": 124
-   
+    "title": "Chesse Paratha",
+    "price": "Rs.200",
+    "description": "Classic delight with 100% real Paratha",
+    "images": require('../assets/icons/paratha.png'),
+    "rating": 3.7,
+    "reviews": 21  
+
    },
   {
     "id": 9,
-    "title": "Classic High-Waisted Athletic Shorts",
-    "price": 43,
-    "description": "Stay comfortable and stylish with our Classic High-Waisted Athletic Shorts. Designed for optimal movement and versatility, these shorts are a must-have for your workout wardrobe. Featuring a figure-flattering high waist, breathable fabric.",
-    "category": "Clothes",
-    "images": "https://i.imgur.com/eGOUveI.jpeg",
-    "currency_code":"USD",
-    "rating": 4.8,
-    "reviews": 124
-  
-  },
+    "title": "Sand\nWiches",
+    "price": "Rs.350",
+    "description": "Classic delight with 100% real",
+    "images": require('../assets/icons/sandwich.png'),
+    "rating": 4.1,
+    "reviews": 11
+},
   {
     "id": 10,
-    "title": "Classic White Crew Neck T-Shirt",
-    "price": 39,
-    "description": "Elevate your basics with this versatile white crew neck tee. Made from a soft, breathable cotton blend, it offers both comfort and durability. Its sleek, timeless design ensures it pairs well with virtually any outfit.",
-    "category": "Clothes",
-    "images": "https://i.imgur.com/axsyGpD.jpeg",
-    "currency_code":"USD",
-    "rating": 4.8,
-    "reviews": 124
+    "title": "French\nFries",
+    "price": "Rs.100",
+    "description": "delight with 100%",
+    "images":  require('../assets/icons/french-fries.png'),
+    "rating": 3.7,
+    "reviews": 12
+
   },
   
   {
     "id": 11,
-    "title": "Sleek White & Orange Wireless Gaming Controller",
-    "price": 69,
-    "description": "Elevate your gaming experience with this state-of-the-art wireless controller, featuring a crisp white base with vibrant orange accents. Designed for precision play, the ergonomic shape and responsive buttons provide maximum comfort and control for endless hours of gameplay.",
-    "category": "Electronics",
-    "images": "https://i.imgur.com/ZANVnHE.jpeg",
-    "currency_code":"USD",
-    "rating": 4.8,
-    "reviews": 124
+    "title": "Drinks",
+    "price": "Rs.150",
+    "description": "cold drink",
+    "images": require('../assets/icons/cold-drinks.png'),
+    "rating": 3.7,
+    "reviews": 7
     
   },
   {
@@ -192,71 +123,5 @@
     "reviews": 124
     
   },
-  {
-    "id": 13,
-    "title": "Efficient 2-Slice Toaster",
-    "price": 48,
-    "description": "Enhance your morning routine with our sleek 2-slice toaster, featuring adjustable browning controls and a removable crumb tray for easy cleaning. This compact and stylish appliance is perfect for any kitchen, ensuring your toast is always golden brown and delicious.",
-    "category": "Electronics",
-    "images": "https://i.imgur.com/keVCVIa.jpeg",
-    "currency_code":"USD",
-    "rating": 4.8,
-    "reviews": 124
-    
-  },
-  {
-    "id": 14,
-    "title": "Sleek Wireless Computer Mouse",
-    "price": 15,
-    "description": "Experience smooth and precise navigation with this modern wireless mouse, featuring a glossy finish and a comfortable ergonomic design. Its responsive tracking and easy-to-use interface make it the perfect accessory for any desktop or laptop setup.",
-    "category": "Electronics",
-    "images": "https://i.imgur.com/w3Y8NwQ.jpeg",
-    "currency_code":"USD",
-    "rating": 4.8,
-    "reviews": 124
-    
-  },
-  {
-    "id": 15,
-    "title": "Sleek Modern Laptop with Ambient Lighting",
-    "price": 43,
-    "description": "Experience next-level computing with our ultra-slim laptop, featuring a stunning display illuminated by ambient lighting. This high-performance machine is perfect for both work and play, delivering powerful processing in a sleek, portable design.",
-    "category": "Electronics",
-    "images": "https://i.imgur.com/OKn1KFI.jpeg",
-    "currency_code":"USD",
-    "rating": 4.8,
-    "reviews": 124
-    
-  },
-  {
-    "id": 16,
-    "title": "Sleek Modern Laptop for Professionals",
-    "price": 97,
-    "description": "Experience cutting-edge technology and elegant design with our latest laptop model. Perfect for professionals on-the-go, this high-performance laptop boasts a powerful processor, ample storage, and a long-lasting battery life, all encased in a lightweight.",
-    "category": "Electronics",
-    "images": "https://i.imgur.com/ItHcq7o.jpeg",
-    "currency_code":"USD",
-    "rating": 4.8,
-    "reviews": 124
-    
-  },  
-  {
-   "id": 17,
-    "title": "Sleek Mirror Finish Phone Case",
-    "slug": "sleek-mirror-finish-phone-case",
-    "price": 27,
-    "description": "Enhance your smartphone's look with this ultra sleek mirror finish phone case.",
-    "category": "Electronics",
-    "images":   "https://i.imgur.com/yb9UQKL.jpeg",
-    "currency_code":"USD",
-    "rating": 4.8,
-    "reviews": 124
- }
-        
-]
-
-
-
-
-
-
+  
+ ]

22861d5630327c4cfbd907975aa0928d7487f0d4
feroz315 <feroz3151@gmail.com>
2026-08-02 00:30:47 +0500

    update

--- Frontend/src/screens/navigation.jsx
+++ Frontend/src/screens/navigation.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import {StatusBar} from 'react-native';
 
 
@@ -14,19 +14,21 @@ import OrderForm from './order';
 import DeliveryMap from './deliveryMap';
 
 
-// Initialize Navigators
-
 const Stack = createStackNavigator();
-const Drawer = createDrawerNavigator();
 
 
-
-const HomwStackNavigator = () => {
+const Navigation = () => {
   return (
-
-   <Stack.Navigator
+    <NavigationContainer>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor="transparent"
+          translucent={true}
+        />
+      <Stack.Navigator
         screenOptions={{headerShown: false}}
         initialRouteName="home">
+        {/* <Stack.Screen name="board" component={BoardScreen} /> */}
         <Stack.Screen name="home" component={Home} />
         <Stack.Screen name="productdetail" component={ProductDetail} />
         <Stack.Screen name="search" component={Search} />
@@ -34,28 +36,11 @@ const HomwStackNavigator = () => {
         <Stack.Screen name="checkout" component={Checkout} />
         <Stack.Screen name="order" component={OrderForm} />
         <Stack.Screen name="map" component={DeliveryMap} />
-        {/* <Stack.Screen name="board" component={BoardScreen} /> */}
-           
+        
+        
       </Stack.Navigator>
-
-    )
-    
-  };
-
-
-const Navigation = () => {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="home">
-        {/* Instead of a regular screen, point the Drawer screen to the Stack Navigator */}
-        <Drawer.Screen 
-          name="home" 
-          component={HomwStackNavigator} 
-          options={{ drawerLabel: 'Home Dashboard' }}
-        />
-       </Drawer.Navigator>
     </NavigationContainer>
-  )
+  );
 };
 
 

e21e8a951b2ff30b2a3d2844c05ec82fe4cc43bd
feroz315 <feroz3151@gmail.com>
2026-08-03 07:09:53 +0500

    update

--- Frontend/src/screens/navigation.jsx
+++ Frontend/src/screens/navigation.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator }  from "@react-navigation/drawer";
 import {StatusBar} from 'react-native';
 
 
@@ -14,35 +14,70 @@ import OrderForm from './order';
 import DeliveryMap from './deliveryMap';
 
 
+// Initialize Navigators
+const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 
 
+//  Create the Nested Stack for the Main Flow
+const HomeStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="home" 
+        component={Home} 
+        options={{ title: 'home' }}
+      />
+      <Stack.Screen 
+        name="search" 
+        component={Search} 
+        options={{ title: 'Global Search' }}
+      />
+    </Stack.Navigator>
+  );
+}
 
 const Navigation = () => {
   return (
-    <NavigationContainer>
-        <StatusBar
-          barStyle="dark-content"
-          backgroundColor="transparent"
-          translucent={true}
+
+<NavigationContainer>
+      <Drawer.Navigator screenOptions={{ headerShown: false }}>
+        {/* Hide Drawer Header because Stack Navigator will manage its own header */}
+        <Drawer.Screen 
+          name="HomeStack" 
+          component={HomeStackNavigator} 
+          options={{ drawerLabel: 'Home Dashboard' }}
         />
-      <Stack.Navigator
-        screenOptions={{headerShown: false}}
-        initialRouteName="home">
-        {/* <Stack.Screen name="board" component={BoardScreen} /> */}
-        <Stack.Screen name="home" component={Home} />
-        <Stack.Screen name="productdetail" component={ProductDetail} />
-        <Stack.Screen name="search" component={Search} />
-        <Stack.Screen name="cart" component={Cart} />
-        <Stack.Screen name="checkout" component={Checkout} />
-        <Stack.Screen name="order" component={OrderForm} />
-        <Stack.Screen name="map" component={DeliveryMap} />
-        
-        
-      </Stack.Navigator>
+      
+      </Drawer.Navigator>
     </NavigationContainer>
+    // <NavigationContainer>
+    //     <StatusBar
+    //       barStyle="dark-content"
+    //       backgroundColor="transparent"
+    //       translucent={true}
+    //     />
+    //   <Stack.Navigator
+    //     screenOptions={{headerShown: false}}
+    //     initialRouteName="home">
+    //     {/* <Stack.Screen name="board" component={BoardScreen} /> */}
+    //     <Stack.Screen name="home" component={Home} />
+    //     <Stack.Screen name="productdetail" component={ProductDetail} />
+    //     <Stack.Screen name="search" component={Search} />
+    //     <Stack.Screen name="cart" component={Cart} />
+    //     <Stack.Screen name="checkout" component={Checkout} />
+    //     <Stack.Screen name="order" component={OrderForm} />
+    //     <Stack.Screen name="map" component={DeliveryMap} />
+        
+        
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+
   );
 };
+
+
+
 
 
 
